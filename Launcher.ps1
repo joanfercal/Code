2c853de330b31253746bea84d112f1b2ae6b18ed
joanfercal <joanfercal@gmail.com>
2023-04-01 00:18:33 -0600

    File Optimizations

--- Launcher.ps1
+++ Launcher.ps1
@@ -4,13 +4,11 @@ function Edge { Start-Process msedge -ArgumentList "--edge-frame", "--app=$($arg
 function RDP { & "mstsc.exe" "$env:userprofile\Documents\VMs\$args" }
 
 $window = New-Object System.Windows.Window -Property @{
-    Title                 = "Run"
     Width                 = 200
     Height                = 200
-    # WindowStartupLocation = "CenterScreen"
     WindowStartupLocation = "Manual"
-    Left                  = [System.Windows.SystemParameters]::PrimaryScreenWidth - $window.Width - 1
-    Top                   = [System.Windows.SystemParameters]::PrimaryScreenHeight - $window.Height - 50
+    Left                  = [System.Windows.SystemParameters]::PrimaryScreenWidth - 201
+    Top                   = [System.Windows.SystemParameters]::PrimaryScreenHeight - 250
     ResizeMode            = "NoResize"
     WindowStyle           = "None"
     Topmost               = $true
@@ -19,61 +17,55 @@ $window = New-Object System.Windows.Window -Property @{
 $grid = New-Object System.Windows.Controls.Grid
 
 for ($i = 0; $i -lt 3; $i++) {
-    $rowDefinition = New-Object System.Windows.Controls.RowDefinition
-    $grid.RowDefinitions.Add($rowDefinition)
-    $columnDefinition = New-Object System.Windows.Controls.ColumnDefinition
-    $grid.ColumnDefinitions.Add($columnDefinition)
+    $grid.RowDefinitions.Add((New-Object System.Windows.Controls.RowDefinition))
+    $grid.ColumnDefinitions.Add((New-Object System.Windows.Controls.ColumnDefinition))
 }
 
-$buttonConfigs = (Invoke-WebRequest -Uri "bit.ly/Automatech-ButtonJSON").Content | ConvertFrom-Json
-$buttons = @()
-$buttons = foreach ($config in $buttonConfigs) {
-    $button = New-Object System.Windows.Controls.Button
-    $button.Content = $config.Name
-    $button.Cursor = [System.Windows.Input.Cursors]::Hand
-    $button.Background = $config.Background
-    $button.BorderThickness = New-Object System.Windows.Thickness $config.BorderThickness
-    $image = New-Object System.Windows.Controls.Image
-    $image.Stretch = "UniformToFill"
-    if ($config.Image.Base64) {
-        $bitmap = New-Object System.Windows.Media.Imaging.BitmapImage
-        $memoryStream = New-Object System.IO.MemoryStream
-        $memoryStream.Write([System.Convert]::FromBase64String($config.Image.Base64), 0, [System.Convert]::FromBase64String($config.Image.Base64).Length)
-        $memoryStream.Position = 0
-        $bitmap.BeginInit()
-        $bitmap.StreamSource = $memoryStream
-        $bitmap.CacheOption = "OnLoad"
-        $bitmap.EndInit()
-        $image.Source = $bitmap
-    } else {
-        $imageUri = "https://raw.githubusercontent.com/<username>/<repository>/<branch>/<path-to-image-file>"
-        $imageUri = $imageUri -replace "<username>", $config.Image.Username `
-            -replace "<repository>", $config.Image.Repository `
-            -replace "<branch>", $config.Image.Branch `
-            -replace "<path-to-image-file>", $config.Image.Path
-        $bitmap = New-Object System.Windows.Media.Imaging.BitmapImage
-        $bitmap.BeginInit()
-        $bitmap.UriSource = New-Object System.Uri($imageUri)
-        $bitmap.EndInit()
-        $image.Source = $bitmap
-    }
-    $button.Content = $image
-    $button.BorderBrush = $config.BorderBrush
-    $button.ToolTip = $config.ToolTip
+$buttonConfigs = Get-Content -Raw -Path "launcher_options.json" | ConvertFrom-Json
 
-    $action = [Scriptblock]::Create($config.Action)
-    $button.Add_Click($action)
+$buttons = foreach ($config in $buttonConfigs) {
+    $image = New-Object System.Windows.Controls.Image -Property @{
+        Stretch = "UniformToFill"
+        Source = if ($config.Image.Base64) {
+            $bitmap = New-Object System.Windows.Media.Imaging.BitmapImage
+            $memoryStream = New-Object System.IO.MemoryStream
+            $memoryStream.Write([System.Convert]::FromBase64String($config.Image.Base64), 0, [System.Convert]::FromBase64String($config.Image.Base64).Length)
+            $memoryStream.Position = 0
+            $bitmap.BeginInit()
+            $bitmap.StreamSource = $memoryStream
+            $bitmap.CacheOption = "OnLoad"
+            $bitmap.EndInit()
+            $bitmap
+        } else {
+            $imageUri = "https://raw.githubusercontent.com/{0}/{1}/{2}/{3}" -f $config.Image.Username, $config.Image.Repository, $config.Image.Branch, $config.Image.Path
+            $bitmap = New-Object System.Windows.Media.Imaging.BitmapImage
+            $bitmap.BeginInit()
+            $bitmap.UriSource = New-Object System.Uri($imageUri)
+            $bitmap.EndInit()
+            $bitmap
+        }
+    }
+
+    $button = New-Object System.Windows.Controls.Button -Property @{
+        Content = $image
+        Cursor = [System.Windows.Input.Cursors]::Hand
+        Background = $config.Background
+        BorderThickness = New-Object System.Windows.Thickness $config.BorderThickness
+        BorderBrush = $config.BorderBrush
+        ToolTip = $config.ToolTip
+    }
+
+    $button.Add_Click([Scriptblock]::Create($config.Action))
     $button
 }
 
-$grid.Children.Clear()
 for ($i = 0; $i -lt $buttons.Count; $i++) {
     $grid.Children.Add($buttons[$i])
     [System.Windows.Controls.Grid]::SetRow($buttons[$i], [math]::Floor($i / 3))
     [System.Windows.Controls.Grid]::SetColumn($buttons[$i], $i % 3)
 }
+
 $grid.Background = "Transparent"
 $window.Background = "Transparent"
-
 $window.Content = $grid
 $window.ShowDialog() | Out-Null
