e29890e330280262658750cc17347b0b8153cf52
joanfercal <joanfercal@gmail.com>
2023-04-01 00:42:32 -0600

    Code cleanup and added a few more checks

--- Uninstaller.ps1
+++ Uninstaller.ps1
@@ -1,60 +1,48 @@
-# Load presentation framework assembly
 Add-Type -AssemblyName PresentationFramework
 
-# XAML code for the GUI
 [xml]$xaml = @"
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-        Title="Select Programs to Uninstall " Height="300" Width="400" ResizeMode="NoResize" WindowStartupLocation="CenterScreen">
+        Title="Select Programs to Uninstall" Height="300" Width="400" ResizeMode="NoResize" WindowStartupLocation="CenterScreen">
     <Grid>
         <Grid.RowDefinitions>
-            <RowDefinition Height="Auto" />
             <RowDefinition Height="Auto" />
             <RowDefinition Height="*" />
             <RowDefinition Height="Auto" />
         </Grid.RowDefinitions>
-        <ScrollViewer Grid.Row="2" Margin="0" VerticalScrollBarVisibility = "Auto">
-        <StackPanel x:Name="SoftwareList" />
+        <ScrollViewer Grid.Row="1" Margin="0" VerticalScrollBarVisibility="Auto">
+            <StackPanel x:Name="SoftwareList" />
         </ScrollViewer>
-        <TextBox Grid.Row="3" Name="SearchBar" Width="295" Height="25" Margin="5,5,5,5" HorizontalAlignment="Left"/>
-        <Button Grid.Row="3" Content="Uninstall" Name="UninstallButton" Width="75" Height="25" Margin="5,5,5,5" HorizontalAlignment="Right" />
+        <TextBox Grid.Row="2" Name="SearchBar" Width="295" Height="25" Margin="5" HorizontalAlignment="Left"/>
+        <Button Grid.Row="2" Content="Uninstall" Name="UninstallButton" Width="75" Height="25" Margin="5" HorizontalAlignment="Right" />
     </Grid>
 </Window>
 "@
 
-# Create GUI from XAML
 $reader = (New-Object System.Xml.XmlNodeReader $xaml)
 $window = [Windows.Markup.XamlReader]::Load($reader)
 $searchBar = $window.FindName('SearchBar')
-
-# Define variables for UI elements
 $softwareList = $window.FindName('SoftwareList')
 $uninstallButton = $window.FindName('UninstallButton')
-# Function to get installed software
+
 function Get-InstalledSoftware {
     $softwarePaths = @(
         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
         'HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
     )
 
-    $installedSoftware = foreach ($path in $softwarePaths) {
-        Get-ItemProperty -Path $path -ErrorAction SilentlyContinue |
+    $softwarePaths | ForEach-Object {
+        Get-ItemProperty -Path $_ -ErrorAction SilentlyContinue |
         Where-Object { $null -ne $_.DisplayName } |
         Select-Object DisplayName, UninstallString, @{Name='IdentifyingNumber'; Expression={$_.PSChildName}}
-    }
-
-    $installedSoftware | Sort-Object -Property DisplayName
+    } | Sort-Object -Property DisplayName
 }
 
-
-# Function to update the checkboxes
 function Update-Checkboxes {
     $searchQuery = $searchBar.Text
     $softwareList.Children.Clear()
 
-    Get-InstalledSoftware |
-    Where-Object { $_.DisplayName -like "*$searchQuery*" } |
-    ForEach-Object {
+    Get-InstalledSoftware | Where-Object { $_.DisplayName -like "*$searchQuery*" } | ForEach-Object {
         $checkbox = New-Object System.Windows.Controls.CheckBox
         $checkbox.Content = $_.DisplayName
         $checkbox.Tag = $_.IdentifyingNumber
@@ -62,7 +50,6 @@ function Update-Checkboxes {
     }
 }
 
-# Function to uninstall selected software
 function Uninstall-SelectedSoftware {
     $softwareList.Children | Where-Object { $_.IsChecked -eq $true } | ForEach-Object {
         $guid = $_.Tag
@@ -78,10 +65,7 @@ function Uninstall-SelectedSoftware {
                 Start-Process "msiexec.exe" -ArgumentList $uninstallArgs -Wait -NoNewWindow
             }
         }
-        
     }
-
-    # Update-Checkboxes
 }
 
 $searchTimer = New-Object System.Windows.Threading.DispatcherTimer
